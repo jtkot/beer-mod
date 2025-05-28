1995e6c0b8ce55009cf89a89a083b96b0b7052c6
Jan Kot <janymalino@gmail.com>
2025-05-28 11:50:10 +0200

    add food properties

--- src/main/java/pl/wiadro24/beermc/ModItems.java
+++ src/main/java/pl/wiadro24/beermc/ModItems.java
@@ -1,32 +1,32 @@
 package pl.wiadro24.beermc;
 
-import java.util.function.Function;
-
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey; // RegistryKey
 import net.minecraft.resources.ResourceLocation; // Identifier
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTabs; // ItemGroups
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.component.Consumables;
 
 public class ModItems {
-	public static final Item FULL_BEER_MUG = register("full_beer_mug", Item::new, new Item.Properties());
-	public static Item register(String name, Function<Item.Properties, Item> itemFactory, Item.Properties properties) {
-		// Create the item key.
-		ResourceKey<Item> itemKey = ResourceKey.<Item>create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(BeerMod.MOD_ID, name));
+	public static final Item FULL_BEER_MUG = register("full_beer_mug", new Item.Properties());
 
-		// Create the item instance.
-		Item item = itemFactory.apply(properties.setId(itemKey));
-
-		// Register the item.
+	public static Item register(String name, Item.Properties properties) {
+		ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM,
+				ResourceLocation.fromNamespaceAndPath(BeerMod.MOD_ID, name));
+		Item item = new Item(properties.setId(itemKey).
+			food(
+				new FoodProperties(1, 0.6f, true), Consumables.MILK_BUCKET));
 		Registry.register(BuiltInRegistries.ITEM, itemKey, item);
 
 		return item;
 	}
+
 	public static void initialize() {
 		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
-			.register((itemGroup) -> itemGroup.prepend(ModItems.FULL_BEER_MUG));
+				.register((itemGroup) -> itemGroup.prepend(ModItems.FULL_BEER_MUG));
 	}
 }
