501d5c650c5ffdf47372094c09e9fbab0d9751b9
Jan Kot <janymalino@gmail.com>
2025-05-28 02:13:56 +0200

    switch to mojang official bindings

--- src/main/java/pl/wiadro24/beermc/ModItems.java
+++ src/main/java/pl/wiadro24/beermc/ModItems.java
@@ -3,30 +3,30 @@ package pl.wiadro24.beermc;
 import java.util.function.Function;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey; // RegistryKey
+import net.minecraft.resources.ResourceLocation; // Identifier
+import net.minecraft.world.item.CreativeModeTabs; // ItemGroups
+import net.minecraft.world.item.Item;
 
 public class ModItems {
-	public static final Item FULL_BEER_MUG = register("full_beer_mug", Item::new, new Item.Settings());
-	public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+	public static final Item FULL_BEER_MUG = register("full_beer_mug", Item::new, new Item.Properties());
+	public static Item register(String name, Function<Item.Properties, Item> itemFactory, Item.Properties properties) {
 		// Create the item key.
-		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BeerMod.MOD_ID, name));
+		ResourceKey<Item> itemKey = ResourceKey.<Item>create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(BeerMod.MOD_ID, name));
 
 		// Create the item instance.
-		Item item = itemFactory.apply(settings.registryKey(itemKey));
+		Item item = itemFactory.apply(properties.setId(itemKey));
 
 		// Register the item.
-		Registry.register(Registries.ITEM, itemKey, item);
+		Registry.register(BuiltInRegistries.ITEM, itemKey, item);
 
 		return item;
 	}
 	public static void initialize() {
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
-			.register((itemGroup) -> itemGroup.add(ModItems.FULL_BEER_MUG));
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
+			.register((itemGroup) -> itemGroup.prepend(ModItems.FULL_BEER_MUG));
 	}
 }
